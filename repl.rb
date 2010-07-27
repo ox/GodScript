b67abecc2f245426bd5aeda930728158458c59ba
Artem Titoulenko <artem.titoulenko@gmail.com>
2010-07-27 17:18:07 -0400

    initial constitution. Does only numbers for now

---
+++ repl.rb
@@ -0,0 +1,33 @@
+def read(str)
+  num = ""
+  sign = 1
+  
+  str.split('').each do |c|
+    #puts "parsing #{c}"
+    c = c.to_s
+    if c == '-':
+      sign = -sign
+    elsif c[/(\d)/]
+      num << $1
+    elsif c == nil || c == "\n"
+      #
+    else
+      raise "we only do numbers for now, not |#{c}|"
+    end
+  end
+  
+  return num.to_i * sign
+end
+
+def god_eval(something)
+  #jsut echo for now
+  return something
+end
+
+def write(something)
+  puts "=> #{something.to_s}"
+end
+
+while 1
+  write(god_eval(read(gets)))
+end
